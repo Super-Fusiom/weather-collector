a1f01cafb3b577a281adc07afa2b2a04aa4a0918
Paul <paul21479@gmail.com>
2022-03-09 15:38:20 +1300

    Adding comments for peace of mind

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+//Change city to whatever you like
+
 var City = "Auckland";
 
-
+//Using the api key to get info on the weather
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + City + "&units=metric&appid=41b67b520170da61ec4cc9ea9764d19a", function(data){
-    console.log(data);
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var temp = Math.floor(data.main.temp);
     var weather = data.weather[0].main;
