015fe9c36f4823129d5d356d24c5b74a9429aad1
Paul <paul21479@gmail.com>
2022-04-09 16:13:57 +1200

    Commenting

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//Change city to whatever you like
+//Change city to whatever you like, This only displays the city weather anyways
 
 var City = "Auckland";
 
-//Using the api key to get info on the weather
+//Using the api key to get info on the weather along with the icons
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + City + "&units=metric&appid=41b67b520170da61ec4cc9ea9764d19a", function(data){
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var temp = Math.floor(data.main.temp);
